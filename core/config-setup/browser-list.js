12b1c1175fdfd75babb5326aee3e6bb446a504ff
SChangkakoti <54186343+SChangkakoti@users.noreply.github.com>
2019-08-19 09:43:54 -0700

    Added files

---
+++ core/config-setup/browser-list.js
@@ -0,0 +1,53 @@
+module.exports = {
+    chrome: {
+        browserName: "Chrome",
+        os: "Windows",
+        os_version: "10",
+        resolution: "1280x1024"
+    },
+    chromeMac: {
+        browserName: "Chrome",
+        os: "OS X",
+        os_version: "Sierra",
+        resolution: "1280x1024"
+    },
+    firefox: {
+        browserName: "Firefox",
+        os: "Windows",
+        os_version: "10",
+        resolution: "1280x1024"
+    },
+    firefoxMac: {
+        browserName: "Firefox",
+        os: "OS X",
+        os_version: "Sierra",
+        resolution: "1280x1024"
+    },
+    ie: {
+        browserName: "internet explorer",
+        browser_version: "11",
+        os: "WINDOWS",
+        os_version: "10",
+        resolution: "1280x1024"
+    },
+    edge: {
+        browserName: "Edge",
+        browser_version: "15.0",
+        os: "Windows",
+        os_version: "10",
+        resolution: "1280x1024"
+    },
+    safari: {
+        browserName: "Safari",
+        os: "OS X",
+        os_version: "Sierra",
+        resolution: "1280x1024"
+    },
+    safari9: {
+        browserName: "Safari",
+        browser_version: "9.1",
+        os: "OS X",
+        os_version: "El Capitan",
+        resolution: "1280x1024"
+    }
+};
